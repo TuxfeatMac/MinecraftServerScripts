979e140a797039d2aaa2b725b9a0b4d9ba28ccc7
TuxfeatMac <joachim.traeuble@gmail.com>
2021-12-16 10:04:49 +0000

    added servertype snapshot

---
+++ snapshot/serverupdate.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#################################################################
+# Name:         serverupdate.sh       Version:      0.3.0       #
+# Created:      05.12.2021            Modified:     16.12.2021  #
+# Author:       Joachim Traeuble                                #
+# Purpose:      fetching the latest paper server 	        #
+#################################################################
+
+#### ADJUST SERVER VARIABELS HERE #######
+#########################################
+SERVER=""                        	#
+VERSION=""				#
+#########################################
+
+#### SET SOME COLOURS ####
+NORMAL=$(tput sgr0)      #
+RED=$(tput setaf 1)      #
+GREEN=$(tput setaf 2)    #
+YELLOW=$(tput setaf 3)   #
+##########################
+
+#### STATIC VARIABELS AND DIRECTORIES ###########
+UDIR=~/$SERVER/scripts/update/			#
+USDIR=~/$SERVER/scripts/update/snapshot/	#
+#################################################
+
+#### CHECK SETUP ################################################
+for VAR in "$SERVER" "$VERSION"					#
+ do								#
+  if [ "$VAR" == "" ]                                  		#
+   then 	                                 	 	#
+    printf "[$RED EXIT $NORMAL] SERVER or VERSION not set!\n"	#
+    exit         	                                	#
+  fi                    	                        	#
+done								#
+#################################################################
+
+#### CHECK DIRS FOLDER OR CREATE IT #############################################
+for DIR in $UDIR $USDIR								#
+ do										#
+  if [ ! -d "$DIR" ]                                                            #
+   then                                                                         #
+    printf "[$RED WARN $NORMAL] $DIR not present\n"                             #
+    read -t 15 -n 1 -p "[ INFO ] create it? [ y / n ] " INPUT                   #
+    if [ "$INPUT" == "y" ]                                                      #
+     then                                                                       #
+      mkdir $DIR                                                                #
+      printf "\n[$GREEN DONE $NORMAL] $DIR created!\n\n"                        #
+     else                                                                       #
+      printf "\n[$YELLOW SKIP $NORMAL] you may run ./serverupdate.sh again\n"	#
+      exit                                                                      #
+    fi                                                                          #
+  fi                                                                            #
+done										#
+#################################################################################
+
+#### CHECK IF SERVER IS RUNNING IF SO INFORM USERS ON SERVER ####################
+RUN=$(screen -list | grep -o "$SERVER")                 			#
+if [ "$RUN" == "$SERVER" ]                              			#
+ then                                                   			#
+  printf "[ INFO ] $SERVER is running... also informing users...\n"		#
+  screen -S $SERVER -X stuff 'say [Info] checking for Server updates...\n'	#
+fi                                                     				#
+#################################################################################
+
+#### GET THE LATEST VERSIONS.JSON ###############################################################
+curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq > versions.json      #
+printf "[$GREEN DONE $NORMAL] fetched latest manifest from Mojang\n"                            #
+LATEST=$(jq -r '.latest.snap' versions.json)							#
+#################################################################################################
+
+#### DOWNLOAD SERVER ONLY IF NONE EXISTENT OR NEWER VERSION IS AVAIABLE #########
+if [ "$VERSION" != "$LATEST" ] || [ ! -f ~/$SERVER/*.jar ]			#
+ then										#
+   if [ "$RUN" == "$SERVER" ]                              			#
+    then                                                   			#
+     printf "[ INFO ] $SERVER is running... also informing users...\n"		#
+     screen -S $SERVER -X stuff 'say [Info] downloading Server updates...\n'	#
+   fi                                                     			#
+   ./serverdownloader.sh $VERSION						#
+   mv vanilla-*.jar $USDIR							# update version numbers ? applay ?
+										#
+   if [ "$RUN" == "$SERVER" ]                                              	#
+    then                                                                   	#
+     screen -S $SERVER -X stuff 'say [Info] Download erfolgreich!\n'		#
+   fi                                                                      	#
+
+ else
+  printf "[$GREEN DONE $NORMAL] Ver.: $VERSION is the latest version.\n"
+  if [ "$RUN" == "$SERVER" ]                                              	#
+   then                                                                   	#
+   screen -S $SERVER -X stuff 'say [Info] Version: $VERSION ist aktuell.\n' 	#
+  fi                                                                      	#
+fi
+
+#### EOF ####
---
+++ snapshot/snapdownloader.sh
@@ -0,0 +1,69 @@
+# !/bin/bash
+#################################################################
+# Name:         snapdownloader.sh     Version:      0.3.0       #
+# Created:      16.12.2021            Modified:     05.12.2021  #
+# Author:       Joachim Traeuble                                #
+# Purpose:      download the server.jar from Mojang based on	#
+#		the provided version                         	#
+#################################################################
+
+#### SET SOME COLOURS ####
+NORMAL=$(tput sgr0)      #
+RED=$(tput setaf 1)      #
+GREEN=$(tput setaf 2)    #
+YELLOW=$(tput setaf 3)   #
+##########################
+
+#### GET THE LATEST VERSIONS.JSON ###############################################################
+curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq > versions.json	#
+printf "[$GREEN DONE $NORMAL] fetched latest manifest from Mojang\n"				#
+LATEST=$(jq -r '.latest.release' versions.json)							#
+LATESTSNAP=$(jq -r '.latest.snapshot' versions.json)						#
+#################################################################################################
+
+#### CHECK IF A VERSION IS PROVIDED ELSE SHOW USAGE OF SCRIPT ###########
+if [ "$1" == "" ]							#
+ then									#
+  printf "[ INFO ] no version provided... try:\n"			#
+  printf "[ INFO ] ./serverdownload.sh <VERSION>\n"			#
+  printf "[ INFO ] latest$GREEN release $NORMAL:    $LATEST\n"		#
+  printf "[ INFO ] latest$YELLOW snapshot $NORMAL:   $LATESTSNAP\n"	#
+  printf "[ INFO ] assuming latest release...\n"			#
+  VERSION=$LATESTSNAP							#
+ else									#
+  VERSION=$1								#
+fi									#
+#########################################################################
+
+#### EXTRACT THE CORRESPONDING VERSION URL ##############################################
+VERSIONURL=$(jq -r ".versions | .[] | select(.id==\"$VERSION\") | .url" versions.json)	#
+if [ "$VERSIONURL" == "" ]								#
+ then											#
+  printf "[$YELLOW INFO $NORMAL] $RED$VERSION$NORMAL does not exist!\n"			#
+  printf "[ INFO ] Try: $GREEN$LATEST$NORMAL or $YELLOW$LATESTSNAP$NORMAL \n"		#
+  rm versions.json									#
+  exit											#
+fi											#
+#########################################################################################
+
+#### GET THE CORRESPONDING VERSION.JSON #########
+curl -s $VERSIONURL | jq > $VERSION.json	#
+#################################################
+
+#### EXTRACT THE DOWNLOAD URL ###################################################
+SERVERURL=$(jq -r '.downloads.server.url' $VERSION.json)			#
+printf "[$GREEN DONE $NORMAL] fetched latest download url from Mojang\n"	#
+#################################################################################
+
+#### DOWNLOAD THE SERVER.JAR ############################################
+printf "[ WAIT ] downloading latest server.jar from Mojang...\n"	#
+wget -O vanilla-$VERSION.jar $SERVERURL -q --show-progress 		#
+printf "[$GREEN DONE $NORMAL] download finished! \n"			#
+#########################################################################
+
+#### CLEAN UP ###########
+rm versions.json	#
+rm $VERSION.json	#
+#########################
+
+#### EOF ####
